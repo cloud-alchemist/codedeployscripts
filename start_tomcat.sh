065ff0ebf15141d841ca4aa9260c22549f1b57fd
sreenath007 <sreenath.real@gmail.com>
2016-07-01 01:26:00 +0530

    Unexpected end of line issue is fixed

--- start_tomcat.sh
+++ start_tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROCESSID_TEMP="processid_temp"
 JAVA_HOME=/usr/java/jdk1.7.0_79/jre
-LOG_FILE="/tmp/log`date +%F`
+LOG_FILE="/tmp/log`date +%F`"
 bash /opt/apache-tomcat-7.0.70/bin/startup.sh
 CHECK_TOMCAT=`ps -ef | grep java | awk -F'-Dcatalina.base=/opt/' '{print $2}' | awk '{ print $1 }' | wc -l`
 
